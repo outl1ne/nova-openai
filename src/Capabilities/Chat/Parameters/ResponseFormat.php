4068d7a68133df61a4c2541b6453ed5f713ba448
Allan Tatter <allan.tatter@gmail.com>
2024-04-03 15:03:05 +0300

    feat: Improve handling of response format in CreateChat
    Ensure response format is properly set in CreateChat by updating the
    response_format argument to use the response format type from
    ResponseFormat class. Also, refactor ResponseFormat methods to return
    self for method chaining.

--- src/Capabilities/Chat/Parameters/ResponseFormat.php
+++ src/Capabilities/Chat/Parameters/ResponseFormat.php
@@ -6,17 +6,19 @@ class ResponseFormat
 {
     public ?array $responseFormat = null;
 
-    public function text(): void
+    public function text(): self
     {
         $this->responseFormat = [
             'type' => 'text',
         ];
+        return $this;
     }
 
-    public function json(): void
+    public function json(): self
     {
         $this->responseFormat = [
             'type' => 'json_object',
         ];
+        return $this;
     }
 }
